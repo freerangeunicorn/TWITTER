4421a320f6a5ef1f6b26a0d6f39a7e2a4dcb05a2
User <user@MacBook-Air-cua-User.local>
2019-10-10 23:19:51 +0700

    phong

--- js/twitter.js
+++ js/twitter.js
@@ -3,14 +3,31 @@ let appState = {
     currentUser: null,
     tweets:[
         {
+            id: 0,
             user: 'anonymous',
             body: null,
             tweetDate: null,
             hashtags: null,
-            likes: null,
-            id: null,
-            
+            isLiked: false,
+            likeCount: 0,   
         }
     ]
 }
 
+function toggle(index) {
+    tweets[index].isLiked = !tweets[index].isLiked;
+    renderTweets();
+  }
+
+// add in render 
+
+// <button class="faButton text-decoration-none mr-2" 
+//             onclick=toggle(${index})>${tweet.isLiked
+//               ? '<i class="fa fa-heart rounded-circle" aria-hidden="true"></i>'
+//               : '<i class="far fa-heart rounded-circle"></i>'
+//               }
+//  </button>
+
+function renderTweets(appState) {
+    
+}
