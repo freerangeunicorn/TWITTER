7864350d7769077ac2906a8a9aa3f54c0e3b0192
haichungcn <haichungcn@gmail.com>
2019-10-10 17:53:40 +0700

    readme

--- js/twitter.js
+++ js/twitter.js
@@ -1,16 +1,31 @@
-let appState = {
-    
-    currentUser: null,
-    tweets:[
-        {
-            user: 'anonymous',
-            body: null,
-            tweetDate: null,
-            hashtags: null,
-            likes: null,
-            id: null,
+// let appState = {
+//     currentUser: null,
+//     tweets:[
+//         {
+//             user: 'anonymous',
+//             body: null,
+//             tweetDate: null,
+//             hashtags: null,
+//             likes: null,
+//             id: null,
             
-        }
-    ]
+//         }
+//     ]
+// }
+
+let textField = document.getElementById('text');
+const allowedChars = 10;
+let count = 0;
+let remaindingChars = 10;
+
+const handleTextField = () => {
+    textField.addEventListener('input', function(){
+        count++;
+        remaindingChars = allowedChars - count;
+        console.log(count, remaindingChars);
+        console.log('user input: ', textField.value);
+    })
 }
 
+
+handleTextField();
