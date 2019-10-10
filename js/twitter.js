6be4fcf8d7c5a0a7e81fc443c6c8c874ddc0959f
haichungcn <haichungcn@gmail.com>
2019-10-10 16:13:44 +0700

    finish design feed

---
+++ js/twitter.js
@@ -0,0 +1,12 @@
+let appState = {
+    currentUser: null,
+    tweets:[
+        {
+            user: 'anonymous',
+            body: null,
+            tweetTime: null,
+            hashtags: null,
+            likes: null,
+        }
+    ]
+}
